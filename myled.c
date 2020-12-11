a71c6a4c7f792e5c78cd958b47f33c293ddab667
sasato0703 <54259090+sasato0703@users.noreply.github.com>
2020-12-11 18:46:06 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -22,8 +22,7 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
         char c;
         if(copy_from_user(&c, buf, sizeof(char)))
                 return -EFAULT;
-
-                printk(KERN_INFO "receive %c\n",c);
+        printk(KERN_INFO "receive %c\n",c);
         if(c == '0'){
                 gpio_base[10] = 1 << 25;
         }else if(c == '1'){
